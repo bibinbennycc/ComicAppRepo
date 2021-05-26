afb96cfdda6f86026e6e11820142f95a83b647e9
bibinbennycc <bibinspc300@gmail.com>
2021-05-26 13:40:19 -0700

    Initial commit Add basic directory structure Add dependencies Add comic activity

---
+++ app/src/main/java/com/codetest/comicapp/feature/ComicActivity.kt
@@ -0,0 +1,12 @@
+package com.codetest.comicapp.feature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codetest.comicapp.R
+
+class ComicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
